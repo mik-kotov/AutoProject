5644057f0752b6e03c01edf46e2e283872fa1b7a
mik-kotov <mikhail.al.kotov@gmail.com>
2024-03-19 16:24:21 +0700

    "First blood"

---
+++ base_page.py
@@ -0,0 +1,11 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver import Remote as RemoteWebDriver
+
+class BasePage():
+    def __init__(self, browser: RemoteWebDriver, url):
+        self.browser = browser
+        self.url = url
+    def open(self):
+        self.browser.get(self.url)
+
