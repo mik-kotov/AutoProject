2999bff3a128a1d1c62fbe43c51720bb8a45b181
mik-kotov <mikhail.al.kotov@gmail.com>
2024-03-19 15:27:23 +0700

    "in the position, commander"

--- conftest.py
+++ conftest.py
@@ -1,18 +1,17 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 
 link = "http://selenium1py.pythonanywhere.com/"
 
 def pytest_addoption(parser):
-    parser.addoption('--language', action='store', default=None,
+    parser.addoption('--language', action='store', default="en",
                      help="Choose language")
 @pytest.fixture(scope="function")
 def browser(request):
     print("\nstart browser for test..")
     browser = webdriver.Chrome()
-    language= request.config.getoption("language")
-    link = f"https://selenium1py.pythonanywhere.com/{language}/catalogue/coders-at-work_207//"
+    language = request.config.getoption("language")
+    link = f"https://selenium1py.pythonanywhere.com/{language}"
     browser.get(link)
     yield browser
     print("\nquit browser..")
