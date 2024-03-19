d9f2353fe1c8ce7d45a61d35c597020b0c7862d9
mik-kotov <mikhail.al.kotov@gmail.com>
2024-03-19 18:12:24 +0700

    "Manipulations with pages"

--- test_main_page.py
+++ test_main_page.py
@@ -1,8 +1,10 @@
-
+from .pages.main_page import MainPage
 from selenium.webdriver.common.by import By
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
+    page = MainPage(browser, link)
+    page.open()  # открываем страницу
+    page.go_to_login_page()
     login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
     login_link.click()
