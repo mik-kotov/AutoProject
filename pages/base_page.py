42c5e6f8ccb215f546d6c2ccd6aa74946c42fc0f
MK <m.kotov@qasquad.com>
2024-03-21 17:31:04 +0700

    Login page methods moved from mane_page to base_page

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,10 +1,10 @@
 import pytest
-from selenium import webdriver
 from selenium.webdriver import Remote as RemoteWebDriver
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from .locators import BasePageLocators
 class BasePage():
     def __init__(self, browser: RemoteWebDriver, url, timeout=10):
         self.browser = browser
@@ -36,5 +36,13 @@ class BasePage():
             return False
 
         return True
+
+    def go_to_login_page(self):
+        link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
+        link.click()
+
+    def should_be_login_link(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
+
 from selenium.common.exceptions import NoAlertPresentException # в начале файла
 
--- test_main_page.py
+++ test_main_page.py
@@ -1,4 +1,5 @@
 from .pages.main_page import MainPage
+from .pages.product_page import ProductPage
 import time
 
 def test_guest_can_go_to_login_page(browser):
@@ -22,27 +23,15 @@ def test_guest_can_see_login_page(browser):
     login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page()
 
-# def test_guest_should_see_login_form_link(browser):
-#     link = "http://selenium1py.pythonanywhere.com/"
-#     page = MainPage(browser, link)
-#     page.open()
-#     page.go_to_login_page()
-#     login_page = LoginPage(browser, link)
-#     time.sleep(3)
-#     login_page.should_be_login_url()
-#
-# def test_guest_should_see_login_form(browser):
-#     link = "http://selenium1py.pythonanywhere.com/"
-#     page = MainPage(browser, link)
-#     page.open()
-#     page.go_to_login_page()
-#     login_page = LoginPage(browser.current_url)
-#     login_page.should_be_login_form()
-#
-# def test_guest_should_see_registration_form(browser):
-#     link = "http://selenium1py.pythonanywhere.com/"
-#     page = MainPage(browser, link)
-#     page.open()
-#     page.go_to_login_page()
-#     login_page = LoginPage(browser.current_url)
-#     login_page.should_be_register_form()
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    time.sleep(2)
